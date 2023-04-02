ae3c9f310134eddb4612fc6cb225b4618f5e9dc8
Michelle Liu <michelleliu@princeton.edu>
2023-04-02 02:03:03 -0400

    Finishing the verbwire API implementation for uploading files

--- securefile.py
+++ securefile.py
@@ -9,6 +9,7 @@ import flask
 from flask import Flask, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
 import os
+import requests
 
 app = flask.Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'static'
@@ -28,19 +29,40 @@ def user():
     response = flask.make_response(html_code)
     return response
 
-@app.route("/upload", methods=["POST"])
+@app.route("/upload", methods=["POST", "GET"])
 def upload():
-    if 'file' not in request.files:
-        flash('No file part')
-        return redirect(request.url)
-    file = request.files['file']
-    if file.filename == '':
-        flash('No selected file')
-        return redirect(request.url)
-    filename = secure_filename(file.filename)
-    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    file.save(filepath)
-    return redirect(url_for('user'))
+    if request.method == "POST":
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        filename = secure_filename(file.filename)
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(filepath)
+
+        url = "https://api.verbwire.com/v1/nft/store/file"
+
+        files = {"filePath": (filepath, open(filepath, "rb"), "image/png")}
+        headers = {
+            "accept": "application/json",
+            "X-API-Key": "sk_live_c07f6ba3-d6fc-4882-a833-51f7f7f6898d"
+        }
+
+        response = requests.post(url, files=files, headers=headers)
+
+        print(response.text)
+        os.remove(filepath)
+
+        return redirect(url_for('upload', link=response.text[29:-3]))
+
+    if request.method == "GET":
+        link = request.args.get("link")
+        html_code = flask.render_template('upload.html', link=link)
+        response = flask.make_response(html_code)
+        return response
 
 if __name__ == '__main__':
     app.run(debug = True)
