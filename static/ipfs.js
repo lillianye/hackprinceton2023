f02cb1af25da52a10ead2bb0ac667679e6626621
Michelle Liu <michelleliu@princeton.edu>
2023-04-01 20:48:25 -0400

    File upload

--- static/ipfs.js
+++ static/ipfs.js
@@ -3,7 +3,6 @@
 function upload_file(filePath){
     const sdk = require('api')('@verbwire/v1.0#4psk2mplfwliyql');
 
-    let filePath = 'feel forte.png'
     sdk.auth('sk_live_c07f6ba3-d6fc-4882-a833-51f7f7f6898d');
     sdk.postNftStoreFile({filePath: filePath},
         {accept: 'application/json'},
